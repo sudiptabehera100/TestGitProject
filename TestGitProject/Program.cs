3de5235815cfce7b3e211d7dcffa28a90536ab6f
TestOrganisation <sudiptabehera100@gmail.com>
2020-06-18 01:39:21 +0530

    test

--- TestGitProject/Program.cs
+++ TestGitProject/Program.cs
@@ -14,6 +14,7 @@ namespace TestGitProject
         [STAThread]
         static void Main()
         {
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
